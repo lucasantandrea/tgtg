80fee89fb83beefe2e3f3f9ec2188998e6c35850
lucasantandrea <santandrea.luca@gmail.com>
2025-01-12 00:02:51 +0100

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,22 +1,16 @@
 import sys
 import os
 
-# Aggiungi il percorso al modulo __main__.py
-#sys.path.append(os.path.dirname(__file__))
-
-import tgtg_scanner.__main__
-
-# Importa il modulo __main__
-#import __main__
 
 from flask import Flask, jsonify
+from tgtg_scanner.__main__ import main
 
 app = Flask(__name__)
 
 @app.route('/run-script', methods=['GET'])
 def run_script():
     # Esegui una funzione dal modulo __main__.py
-    risultato = __main__.main()
+    risultato = main()
     return jsonify({"message": "Script eseguito con successo!", "result": risultato})
 
 if __name__ == '__main__':

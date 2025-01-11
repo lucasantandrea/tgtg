739e5d7acb69742d54568ffaac68e428af62a189
lucasantandrea <santandrea.luca@gmail.com>
2025-01-11 23:26:57 +0100

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -18,5 +18,6 @@ def run_script():
     return jsonify({"message": "Script eseguito con successo!", "result": risultato})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 

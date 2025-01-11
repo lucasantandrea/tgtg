e7f30fb59d581b839136ad5cd47b30e84ba2582a
lucasantandrea <santandrea.luca@gmail.com>
2025-01-11 23:43:54 +0100

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -8,16 +8,17 @@ sys.path.append(os.path.dirname(__file__))
 import __main__
 
 from flask import Flask, jsonify
+from tgtg_scanner import run 
 
 app = Flask(__name__)
 
 @app.route('/run-script', methods=['GET'])
 def run_script():
     # Esegui una funzione dal modulo __main__.py
-    risultato = __main__.main()
+    # risultato = __main__.main()
+    risultato = run()
     return jsonify({"message": "Script eseguito con successo!", "result": risultato})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
